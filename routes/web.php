35560b19a2186b620278e75d5087bf199ad99a1b
dcy <1046892339@qq.com>
2017-03-22 15:35:00 +0800

    adwd

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/** @noinspection PhpUndefinedMethodInspection */
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');

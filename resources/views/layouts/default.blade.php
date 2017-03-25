26f1485e8b9dbd01ee8db95361ff4d5bafb12729
dcy <1046892339@qq.com>
2017-03-25 16:25:14 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,25 +2,16 @@
 <html>
 <head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset("/css/app.css") }}">
 </head>
 <body>
-<header class="navbar navbar-fixed-top navbar-inverse">
-    <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-            <a href="/" id="logo">Sample App</a>
-            <nav>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="{{ asset('/help') }}">帮助</a></li>
-                    <li><a href="#">登录</a></li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-</header>
+@include('layouts._header')
 
 <div class="container">
-    @yield('content')
+    <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </div>
 </body>
 </html>
